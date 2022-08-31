5527887aa82cb99c61fc21b997ff33d346c81a95
vivian <vivian.v18723@gmail.com>
2022-08-31 15:46:23 +0100

    create form & button components import both components

--- src/components/Repos/index.jsx
+++ src/components/Repos/index.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 
-import "./Repos.css";
+import Buttons from "./Buttons";
+import Form from "./Form";
 
 const Repos = () => {
   return (
     <>
-      <form action="#" method="get" id="repoFormBg">
-        <i className="fa-solid fa-magnifying-glass search_button"></i>
-
-        <input
-          type="text"
-          placeholder="Find a repository..."
-          className="repo_search_input"
-        />
-      </form>
+      <Form />
+      <Buttons />
     </>
   );
 };
