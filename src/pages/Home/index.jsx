477a9c3be5d2c9fbc1996a9b4d3f7c5df6604257
madCakes <jason.pbrooks@hotmail.com>
2022-08-31 12:43:58 +0100

    add handleSearch, add searchString to state

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,13 +6,14 @@ import { Search } from "../../components";
 const Home = () => {
   const [repoData, setRepoData] = useState("");
   const [repoNames, setRepoNames] = useState("");
+  const [searchString, setSearchString] = useState("madcakes");
 
   useEffect(() => {
     (async () => {
       try {
         const user = "madcakes";
         const { data } = await axios.get(
-          `https://api.github.com/users/${user}/repos`
+          `https://api.github.com/users/${searchString}/repos`
         );
 
         setRepoData(data);
@@ -30,33 +31,39 @@ const Home = () => {
         console.log(err);
       }
     })();
-  }, []);
+  }, [searchString]);
+
+  function handleSearch(userInput) {
+    setSearchString(userInput);
+  }
 
   return (
     <>
       <h1>Home</h1>
-      <Search />
+      <Search handleSearch={handleSearch} />
     </>
   );
 };
 
 export default Home;
 
-const useAxiosPost = (url, payload) => {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState("");
-  const [loaded, setLoaded] = useState(false);
-  useEffect(() => {
-    (async () => {
-      try {
-        const response = await axios.post(url, payload);
-        setData(response.data);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoaded(true);
-      }
-    })();
-  }, []);
-  return { data, error, loaded };
-};
+// const useAxiosPost = (url, payload) => {
+//   const [data, setData] = useState(null);
+//   const [error, setError] = useState("");
+//   const [loaded, setLoaded] = useState(false);
+//   useEffect(() => {
+//     (async () => {
+//       try {
+//         const response = await axios.post(url, payload);
+//         setData(response.data);
+//       } catch (error) {
+//         setError(error.message);
+//       } finally {
+//         setLoaded(true);
+//       }
+//     })();
+//   }, []);
+//   return { data, error, loaded };
+// };
+
+// const url = `https://github.com/${inputValue}?tab=repositories`
