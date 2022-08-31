6d0f0e9f503f5905e55d8547d45c2f241e98c6a3
vivian <vivian.v18723@gmail.com>
2022-08-31 14:23:38 +0100

    create, style & export Repo component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { MainHeader, Search } from "../../components";
+import { MainHeader, Repos, Search } from "../../components";
 
 const Home = () => {
   return (
     <>
       <MainHeader />
       <main>
+        <Repos />
         <Search />
       </main>
     </>
