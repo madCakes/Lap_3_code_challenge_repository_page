e172bc5d0edf038f9606d6cdbcf39175c1d98b4f
vivian <vivian.v18723@gmail.com>
2022-08-31 17:03:24 +0100

    create, style & import RepoLinks component

---
+++ src/components/Repos/RepoLinks/index.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+import "./RepoLinks.css";
+
+const RepoLinks = () => {
+  return (
+    <>
+      <h4>Repositories</h4>
+
+      <section id="repoLinksBg">
+        <ul id="listBg">
+          <li>
+            <a href="#">bhatia-cohort</a>
+            <span className="public_private">Public</span>
+          </li>
+          <li>Cohort repository for Bhatia</li>
+        </ul>
+
+        <div id="iconBg">
+          <span>
+            <i class="fa-solid fa-circle"></i> JavaScript
+          </span>
+          <span>
+            <i class="fa-solid fa-code-fork"></i>1
+          </span>
+          <span>
+            <i class="fa-solid fa-star"></i>0
+          </span>
+          <span>
+            <i class="fa-solid fa-circle-dot"></i>1
+          </span>
+          <span>
+            <i class="fa-solid fa-code-pull-request"></i>1
+          </span>
+          <span>Updated 20 days ago</span>
+        </div>
+      </section>
+
+      <footer id="repoLinksFooterBg">
+        <button>
+          <i class="fa-solid fa-chevron-left"></i> Previous
+        </button>
+        <button>
+          Next <i class="fa-solid fa-chevron-right"></i>
+        </button>
+      </footer>
+    </>
+  );
+};
+
+export default RepoLinks;
--- src/components/Repos/index.jsx
+++ src/components/Repos/index.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import Buttons from "./Buttons";
 import Form from "./Form";
+import RepoLinks from "./RepoLinks";
 
 const Repos = () => {
   return (
     <>
       <Form />
       <Buttons />
+      <RepoLinks />
     </>
   );
 };
