c4e2b3fa823758e2fe178ccd684ec36e3c97b524
madCakes <jason.pbrooks@hotmail.com>
2022-08-31 15:47:40 +0100

    update filename

--- src/context.jsx
+++ src/context.jsx
@@ -3,7 +3,15 @@ import { createContext, useState } from "react";
 const RepoContext = createContext();
 
 export function RepoProvider({ children }) {
-  return <RepoContext.Provider value={d}>{children}</RepoContext.Provider>;
+  return (
+    <RepoContext.Provider
+      value={{
+        item: 1,
+      }}
+    >
+      {children}
+    </RepoContext.Provider>
+  );
 }
 
 export default RepoProvider;
