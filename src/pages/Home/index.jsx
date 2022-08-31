82f7cbdb7f7c7851375d8df492abd49800ed598e
vivian <vivian.v18723@gmail.com>
2022-08-31 19:27:17 +0100

    import RepoCard component to see content on screen

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-import { Header, Repos, Search } from "../../components";
+import { Header, RepoCard, Repos, Search } from "../../components";
 
 const Home = () => {
   const [repoData, setRepoData] = useState([]);
